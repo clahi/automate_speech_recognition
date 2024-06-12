b72d9a110da14c1ee23a5f1fe10e45f04803c874
Abdala <clahimoha1000@gmail.com>
2024-06-12 14:35:08 +0300

    Created the first lambda function which will be triggered by an upload to an s3 bucket

---
+++ lambda.tf
@@ -0,0 +1,98 @@
+resource "aws_iam_role" "lambdaRole" {
+  name = "lambdaRole"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17"
+    "Statement" : [
+      {
+        "Effect" : "Allow"
+        "Action" : [
+          "sts:AssumeRole"
+        ]
+        "Principal" : {
+          "Service" : [
+            "lambda.amazonaws.com"
+          ]
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambdaS3Policy" {
+  name = "lambdaS3Policy"
+  policy = jsonencode({
+    Version : "2012-10-17"
+    Statement : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "arn:aws:logs:*:*:*"
+      },
+      {
+        Effect : "Allow"
+        Action : [
+          "s3:GetObject"
+        ]
+        Resource : "arn:aws:s3:::my-source-bucket-76sdf700/*"
+      },
+      {
+      "Effect": "Allow",
+      "Action": [
+        "transcribe:StartTranscriptionJob"
+      ],
+      "Resource": "*"
+    }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambdaRolePolicyAttachment" {
+  policy_arn = aws_iam_policy.lambdaS3Policy.arn
+  roles      = [aws_iam_role.lambdaRole.name]
+  name       = "lambdaRolePolicyAttachment"
+}
+
+data "archive_file" "lambdaFile" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambdaFunction"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "resizeImagesLambda" {
+  role             = aws_iam_role.lambdaRole.arn
+  filename         = data.archive_file.lambdaFile.output_path
+  source_code_hash = data.archive_file.lambdaFile.output_base64sha256
+  function_name    = "resizeImagesLambda"
+  timeout          = 60
+  runtime          = "python3.7"
+  handler          = "lambda.lambda_handler"
+
+  environment {
+    variables = {
+      DEST_BUCKET = aws_s3_bucket.myDestiBucket.id
+    }
+  }
+}
+
+resource "aws_lambda_permission" "resizeImagePermission" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.resizeImagesLambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.mySourceBucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucketNotification" {
+  bucket = aws_s3_bucket.mySourceBucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.resizeImagesLambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.resizeImagePermission]
+}
